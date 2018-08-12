4edb8ae650a56d23278e2a8dcb736b7ba8ec9c98
Gabriel Dias <ti.gabriel.dias@gmail.com>
2018-08-12 20:28:44 -0300

    Alterações no projeto. Algumas classes do domain... sem testar

---
+++ src/main/java/br/com/drogaria/domain/GenericDomain.java
@@ -0,0 +1,26 @@
+package br.com.drogaria.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+@SuppressWarnings("serial")
+public class GenericDomain implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+}
